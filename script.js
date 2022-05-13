c6a7f5c7cab969f37332cb64477898ff6358a755
Adrastea <Adrastea966@yahoo.com>
2022-05-13 02:41:22 -0300

    Se agregó un array al proyecto

--- script.js
+++ script.js
@@ -1,45 +1,71 @@
 // Simulador de carrito de compras
 
+let listaProductos = prompt("¡Hola! escribí DO para ver los productos disponibles")
+
+const bebidas = [{
+    bebida: "Leche de coco",
+    sabor: "coco",
+    precioProducto: 360
+}, {
+    bebida: "Refresco de frutilla",
+    sabor: "frutilla",
+    precioProducto: 475
+}, {
+    bebida: "Refresco de sandía",
+    sabor: "sandía",
+    precioProducto: 350
+}, {
+    bebida: "Refresco de melón",
+    sabor: "melón",
+    precioProducto: 470
+}, ];
+
+let listaBebidasPrecios = ""
+
+bebidas.forEach((refresco) => {
+    listaBebidasPrecios += refresco.bebida + "\n"
+    listaBebidasPrecios += "$" + refresco.precioProducto + "\n"
+})
+
+alert(listaBebidasPrecios)
+
 let productos = prompt("Elige tu producto: \n 1. Leche de coco \n 2. Refresco de frutilla \n 3. Refresco de sandía \n 4. Refresco de melón  \n Escribe ESC para finalizar la compra")
 
 function carrito() {
     switch (productos) {
         case "1":
             alert('Leche de coco se agregó al carrito');
-            precioFinal(280)
+            precioFinal(360)
             break
 
         case "2":
             alert('Refresco de frutilla se agregó al carrito');
-            precioFinal(350)
+            precioFinal(475)
             break
 
         case "3":
             alert('Refresco de sandía se agregó al carrito');
-            precioFinal(300)
+            precioFinal(350)
             break
 
         case "4":
             alert('Refresco de melón se agregó al carrito');
-            precioFinal(450)
+            precioFinal(470)
 
         default:
             alert('Aún no tenemos ese refresco en nuestra tienda :(');
     }
 }
 
-// Le sumo una cantidad al valor del precio total.
-
 let precioTotal = 0;
+
 function precioFinal(precio) {
     precioTotal += precio
 }
 
-do{
+do {
     carrito()
     productos = prompt("Elige tu producto: \n 1. Leche de coco \n 2. Refresco de frutilla \n 3. Refresco de sandía \n 4. Refresco de melón  \n Escribe ESC para finalizar la compra")
-}while (productos != 'ESC') {
-    alert('El precio total de tu compra es: ' + precioTotal)
+} while (productos != 'ESC') {
+    alert('Compra finalizada. El precio total de su compra es : $' + precioTotal)
 }
-    
-
